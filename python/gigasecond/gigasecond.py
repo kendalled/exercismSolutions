6b54acd667d3914fe237820156c28fa3cf1af634
Kendall Jackson <quailpecker@gmail.com>
2017-07-15 06:15:08 -0400

    Added solution to 'gigasecond'
    I was overthinking it. All I needed to do was use datime.timedelta()

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,12 @@
-def add_gigasecond():
-    pass
+# Kendall Jackson
+# 07/15/17
+# This program solves exercism, python problem 'gigasecond'
+# timedelta takes argument 'seconds' rather than default - 'days'
+
+import datetime as dt
+
+def add_gigasecond(x):
+   return x + dt.timedelta(seconds=10**9)
+
+
+    
